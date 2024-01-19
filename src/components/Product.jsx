43a13801a35b5a2dfd76eb2fbcef8567fac62a3f
Robert Chege <rchege737@gmail.com>
2024-01-19 22:05:00 +0300

    chore: initialize ShoppingCartBody

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import ProductDetails from './ProductDetails'
+import IncrementButton from './IncrementButton'
+import DecrementButton from './DecrementButton'
+
 
 export default function Product() {
   return (
     <div>Product
       <h3>Image</h3>
       <ProductDetails />
-      <IncremenntButton />
+      <IncrementButton />
       <DecrementButton />
       <h3>Price</h3>
     </div>
--- src/components/ShoppingCartBody.jsx
+++ src/components/ShoppingCartBody.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Product from './Product'
+
+
 
 export default function ShoppingCartBody() {
   return (
