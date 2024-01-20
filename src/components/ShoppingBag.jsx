ab4f46435f4fb352681815bb78f827376f158796
Elaine Muhombe <emuhombe@gmail.com>
2024-01-20 18:51:09 +0300

    chore: Add css file and Update the shopping cart Header files

--- src/components/ShoppingBag.jsx
+++ src/components/ShoppingBag.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FiShoppingBag } from "react-icons/fi";
+import "../styles.css";
 
 export default function ShoppingBag() {
   return (
     <div>
-        <FiShoppingBag />
+        <FiShoppingBag className="bag" />
     </div>
   )
 }
