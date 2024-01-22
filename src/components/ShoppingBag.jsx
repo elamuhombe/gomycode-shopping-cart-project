10f9b3cf5c0e6e128359c1a8f0eeafede141dc67
Elaine Muhombe <emuhombe@gmail.com>
2024-01-22 20:11:20 +0300

    chore: update shopping cart header files

--- src/components/ShoppingBag.jsx
+++ src/components/ShoppingBag.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { FiShoppingBag } from "react-icons/fi";
 import "../styles.css";
 
-export default function ShoppingBag() {
+export default function ShoppingBag({items}) {
   return (
-    <div>
+    <div className="bag-container">
+      
+        <div className="circle">{items.length}</div>
         <FiShoppingBag className="bag" />
     </div>
   )
