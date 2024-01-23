185497229e08cc54f17991c6c51ac96f93116d45
Robert Chege <rchege737@gmail.com>
2024-01-23 20:14:16 +0300

    chore: add event handler for buttons

--- src/components/IncrementButton.jsx
+++ src/components/IncrementButton.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { GoChevronUp } from "react-icons/go";
 
 
-export default function IncrementButton() {
+export default function IncrementButton({ onClick }) {
+  const handleButtonClick = () => {
+    // Call the onClick function passed as a prop
+    if (onClick) {
+      onClick();
+    }
+    
+  };
+
   return (
-    <div>
-      <GoChevronUp className="increment" />
-    </div>
-  )
+    <button className="increment" onClick={handleButtonClick}>
+      <GoChevronUp />
+    </button>
+  );
 }
- 
